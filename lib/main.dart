8c9579255dc3fe59f8a4fa5268e88604cf73622e
leandororochaadm <leandrorochaadm@gmail.com>
2020-03-28 06:06:18 -0400

    Tela home, app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -7,11 +8,34 @@ const request =
     "https://api.hgbrasil.com/finance?format=json-cors&key=60df7604";
 
 void main() async {
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+  print(await getData());
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Container(),
+    home: Home(),
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor de moedas \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
 }
